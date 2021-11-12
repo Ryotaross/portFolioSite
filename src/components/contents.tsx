2c7c4fea132fbf4f53b5640a61407f0d1f3c3d84
Ryotaro Shimizu <ryotaro_625@icloud.com>
2021-11-12 22:04:41 +0900

    History要素追加

--- src/components/contents.tsx
+++ src/components/contents.tsx
@@ -16,3 +16,13 @@ export const MainText = styled.p `
   margin: 0 0 1.25em;
   line-height: 1.625;
 `;
+
+export const Flex = styled.div `
+  display: flex;
+`;
+
+export const Month = styled.h4 `
+  display: block;
+  margin: 0 2.5em 0 1.25em;
+  line-height: 1.625;
+`;
--- src/components/historyMain.tsx
+++ src/components/historyMain.tsx
@@ -3,12 +3,28 @@ import '../App.css';
 import { Main } from './contents';
 import { MainName } from './contents';
 import { MainText } from './contents';
+import { Flex } from './contents';
+import { Month } from './contents';
 
 function HistoryMain() {
   return (
     <Main>
       <MainName>Social</MainName>
       <MainText>アカウントです。</MainText>
+      <Flex>
+        <Month>1月</Month>
+        <div>
+        <MainText>プログラミング開始</MainText>
+        <MainText>プログラミング開始</MainText>
+        </div>
+      </Flex>
+      <Flex>
+        <Month>1月</Month>
+        <div>
+        <MainText>プログラミング開始</MainText>
+        <MainText>プログラミング開始</MainText>
+        </div>
+      </Flex>
     </Main>
   );
 }
